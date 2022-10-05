3bf717e473ad72dea7e91b9337393387b4b4cde8
lhash5175 <lhash5175@hotmail.com>
2022-10-04 22:17:32 -0400

    added sink number clue

--- Interactables/Numpad.gd
+++ Interactables/Numpad.gd
@@ -14,6 +14,11 @@ func _ready():
 	pass # Replace with function body.
 	
 
+func _physics_process(delta):
+	if Input.is_action_just_pressed("cancel"):
+		queue_free();
+	
+
 func reset_state():
 	entry_arr.clear();
 	entry_str = "";
